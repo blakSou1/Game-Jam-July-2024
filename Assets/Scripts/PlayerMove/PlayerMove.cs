c4bcffe525424707347feff76560f983ecba9df2
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2024-06-30 19:16:14 +0500

    сюдаа реанимация н2 ФФФФФ

--- Assets/Scripts/PlayerMove/PlayerMove.cs
+++ Assets/Scripts/PlayerMove/PlayerMove.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+
+открытый  класс  PlayerMovement : MonoBehaviour  
 {
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float runSpeed = 5f;
     private Rigidbody2D rb;
-    private Animator an;
-
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        an = GetComponent<Animator>();
     }
     private void Update()
     {
@@ -20,38 +19,14 @@ public class PlayerMove : MonoBehaviour
         bool statusRun = Input.GetKey(KeyCode.LeftShift);
         Move(hInput, statusRun);
     }
-    bool facR = true;
-    private void Flip()
-    {
-        facR = !facR;
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
-    }
     private void Move(float input, bool run)
     {
         if(input != 0)
         {
-            if(input > 0 && !facR)
-                Flip();
-            else if(input < 0 && facR)
-                Flip();
-            an.SetBool("Paste", true);
             if (!run)
-            {
-                an.SetBool("Run", false);
                 rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
-            }
-            else
-            {
-                an.SetBool("Run", true);
+            else 
                 rb.velocity = new Vector2(input * runSpeed, rb.velocity.y);
-            }
-        }
-        else
-        {
-            an.SetBool("Run", false);
-            an.SetBool("Paste", false);
         }
     }
 }
